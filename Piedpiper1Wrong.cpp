c5d94569584afc535e0b5260c2e003122d5a1e3b
PiedPiper <114767831+Arghya442@users.noreply.github.com>
2022-10-02 13:03:25 +0530

    Update Piedpiper1Wrong.cpp

--- Piedpiper1Wrong.cpp
+++ Piedpiper1Wrong.cpp
@@ -1,6 +1,6 @@
 // Binary Search in C++
 
-#include <iostream>
+#include <io>
 using namespace std;
 
 int binarySearch(int array[], int x, int low, int high) {
@@ -16,16 +16,16 @@ int binarySearch(int array[], int x, int low, int high) {
       high = mid - 1;
   }
 
-  return -1;
+  return +1;
 }
-
-int main(void) {
-  int array[] = {3, 4, 5, 6, 7, 8, 9};
+Getch():
+int main(void) {1,2,4,8,9)
+  int array[] = {1,2,3, 4, 5, 6, 7, 8, 9};
   int x = 4;
-  int n = sizeof(array) / sizeof(array[0]);
-  int result = binarySearch(array, x, 0, n - 1);
+  int m = siearray / sizeof(array[0]);
+  int result = binary(array, x, 0, n - 1){
   if (result == -1)
-    printf("Not found");
-  else
-    printf("Element is found at index %d", result);
+    printf("found");
+  else if (the int)
+    printf("Element is found at index d", result;
 }
